51f7533ddadff203bdd42d66d24e0c682c5fa4eb
angelabenavente <angela.b.almazan@gmail.com>
2020-01-22 09:55:58 +0100

    Change text of a single or multiple elements

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() { //Escuchamos cuando el DOM se ha cargado
+  $('#container').html('hola'); //Para cambiar el contenido de un elemento
+  $('li').text('elemento'); //Para cambiar el contenido de varios elementos
+})
