b8a4ce915dd477e506e96cbe5d81b3c53ebadafd
angelabenavente <angela.b.almazan@gmail.com>
2020-01-22 10:35:26 +0100

    Select by atribute

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 $(document).ready(function() { //Escuchamos cuando el DOM se ha cargado
-  $('#container').html('<h1>hola</h1>'); //Añade nodo dentro de un elemento (apndchild)
-  /* $('li').text('elemento'); //Texto en varios elementos (innerhtml) */
-  /* $('li,ul').html('<p>elemento<p>'); //Seleccionar varios elementos. */
-  /* $('#list li').addClass('highlighted'); // Selec. hijos directos o no. */
-  $('#list > li').addClass('highlighted'); // Selec. hijos directos.
+
+  /* Añade nodo dentro de un elemento (apndchild)
+  $('#container').html('<h1>hola</h1>'); */
+
+  /* Texto en varios elementos (innerhtml)
+  $('li').text('elemento'); */
+
+  /* Seleccionar varios elementos
+  $('li,ul').html('<p>elemento<p>'); */
+
+  /* Selec. hijos directos o no.
+  $('#list li').addClass('highlighted'); */
+
+  /* Selec. hijos directos. 
+  $('#list > li').addClass('highlighted'); */
+
+  /* Selec. por atributo required
+  $('input[required]').addClass('important') */
+
+  /* Selec. por el valor del atributo
+  $('input[placeholder="jon_nieve@gmail.com"]').addClass('highlighted') */
+
+  /* Selec. si contiene una palabra (distingue enre mayusculas y minúsculas) */
+  $('input[placeholder*="Jon"]').addClass('highlighted') //
 })
